19082e0a77b915c61050de9b303c26974de10648
phathung2001 <phathung2001@gmail.com>
2020-08-26 22:58:54 +0700

    Update ver 3.1

--- BasicDefinitions.py
+++ BasicDefinitions.py
@@ -19,7 +19,7 @@ import pandasql
 from tabulate import tabulate
 
 # Bot info
-ver = "3.0"
+ver = "3.1"
 date = "16/08/2020"
 runningOnHeroku = (os.getenv("RUNNING_ON_HEROKU") == "1")
 print("Bot started.")

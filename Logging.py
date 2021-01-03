18789a47657570df14c59f439007fd577fd74caf
hungngocphat01 <hungngocphat01@gmail.com>
2021-01-04 01:39:38 +0700

    Add unpack support for log func

--- Logging.py
+++ Logging.py
@@ -3,11 +3,14 @@ from os import replace
 
 runtime_logs = []
 
-def log(obj):
-    if isinstance(obj, str):
-        obj = str(obj).replace("```", "@@@")
-    print(obj)
-    runtime_logs.append(obj)
+def log(*args):
+    objs = list(args)
+    for i in range(0, len(objs)):
+        if isinstance(objs[i], str):
+            objs[i] = str(objs[i]).replace("```", "@@@")
+    msg = " ".join(objs)
+    print(msg)
+    runtime_logs.append(msg)
 
 def command_log(ctx):
     log(f"In channel {ctx.message.channel}, {str(ctx.message.author)} invoked command: {ctx.message.content}")

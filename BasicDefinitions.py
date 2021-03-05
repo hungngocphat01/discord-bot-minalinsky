4efdfbdee7803f5e2a103e1433e7d764ab1d8a8d
hungngocphat01 <hungngocphat01@gmail.com>
2021-03-05 13:39:30 +0700

    Update version. Remove excel-based db

--- BasicDefinitions.py
+++ BasicDefinitions.py
@@ -20,8 +20,8 @@ from tabulate import tabulate
 from Logging import *
 
 # Bot info
-ver = "3.9.1"
-date = "19/01/2021"
+ver = "4.0"
+date = "05/03/2021"
 runningOnHeroku = (os.getenv("RUNNING_ON_HEROKU") == "1")
 
 log(f"Minalinsky Discord Bot v{ver}")
@@ -30,39 +30,12 @@ log(f"Scripted in Python 3.")
 log(f"(c) 2020-2021 Hung Ngoc Phat")
 log("================================")
 
-#############  Read the database #############
-# Import database
-eventsdb = None
-
-try:
-    eventsdb = pd.read_excel("events.xls")
-except FileNotFoundError:
-    log("Database file not found. Please check again.")
-
-# Init pandasql
-pquery = lambda queryStr: pandasql.sqldf(queryStr, globals())
-query = lambda queryStr: tabulate(pquery(queryStr), showindex = False, headers = [])
-
 #############  Supporting functions #############
 
 def getTime(zone):
     now_utc = datetime.now(pytz.timezone("UTC"))
     now_zone = now_utc.astimezone(pytz.timezone(zone))
     return now_zone.strftime("%d/%m/%Y, %H:%M:%S")
-
-def similarityBetween(str1, str2):
-    same = 0
-    n1 = len(str1)
-    n2 = len(str2)
-
-    if n1 > n2:
-        Range = range(0, n2)
-    else:
-        Range = range(0, n1)
-    for i in Range:
-        if str1[i] == str2[i]:
-            same += 1
-    return same
 
 #############  Init bot ############# 
 TOKEN = str(os.getenv("BOT_TOKEN"))

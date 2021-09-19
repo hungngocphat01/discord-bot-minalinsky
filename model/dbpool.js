74565582c09c972f348db662a463039743218241
hungngocphat01 <hungngocphat01@gmail.com>
2021-09-20 00:13:18 +0700

    Add rejectUnauthorized

--- model/dbpool.js
+++ model/dbpool.js
@@ -4,6 +4,9 @@ const connectionString = process.env.DATABASE_URL;
 console.log('Database is being initialized...');
 let pool = new Pool({
     connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.once('connect', client => {

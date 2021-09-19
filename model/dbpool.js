82591b9b7ff901312a3b0f39c1244a25a277e067
hungngocphat01 <hungngocphat01@gmail.com>
2021-09-20 00:09:26 +0700

    Change DB_URL to DATABASE_URL

--- model/dbpool.js
+++ model/dbpool.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const connectionString = process.env.DB_URL;
+const connectionString = process.env.DATABASE_URL;
 
 console.log('Database is being initialized...');
 let pool = new Pool({
